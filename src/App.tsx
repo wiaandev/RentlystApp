3e575e47858639b617c2d765e3c6665ec96efbb6
Wiaan Duvenhage <wiaan@stackworx.io>
2025-06-22 08:28:33 +0200

    fix: relay environment

--- src/App.tsx
+++ src/App.tsx
@@ -5,42 +5,44 @@ import {LocalizationProvider} from '@mui/x-date-pickers';
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
 import {theme} from "./Theme/theme.tsx";
 import {Loading} from "./Components/Loading/Loading.tsx";
-import environment from "./Hooks/useRelayEnv.ts";
+import {useRelayEnv} from "./Hooks/useRelayEnv.ts";
 import {RelayEnvironmentProvider} from "react-relay";
 import {AuthContextController} from "./Context/AuthContext.tsx";
+import {IEnvironment} from "relay-runtime";
 export function App() {
-    // https://dev.azure.com/Stackworx/Book-A-Pharmacy/_search?action=contents&text=useRelayEnv&type=code&lp=code-Project&filters=ProjectFilters%7BBook-A-Pharmacy%7DRepositoryFilters%7BBookAPharmacy%7D&pageSize=25&result=DefaultCollection/Book-A-Pharmacy/BookAPharmacy/GBmain//src/ClientApp/src/%40BookAPharmacy/BookAPharmacy/Hooks/useRelayEnv.ts
-    // Extra Resource: https://relay.dev/docs/guides/network-layer/
-    // const {environment, version, resetEnvironment} = environment;
+    const {environment, version, resetEnvironment} = useRelayEnv();
 
     return (
         <AppWithEnvironment
-            // key={version}/
-            // environment={environment}
-            // resetEnvironment={resetEnvironment}
+            key={version}
+            environment={environment}
+            resetEnvironment={resetEnvironment}
         />
     );
 }
 
 function AppWithEnvironment(
-
+    {environment, resetEnvironment}: {
+        environment: IEnvironment;
+        resetEnvironment: () => void;
+    },
 ) {
     return (
         <RelayEnvironmentProvider environment={environment}>
-                <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='en-gb'>
-                    <ThemeProvider theme={theme}>
-                        <CssBaseline />
-                        <React.Suspense fallback={<Loading />}>
-                            {/*<SnackBarProvider>*/}
-                            {/*    <ModalContextProvider>*/}
-                                    <AuthContextController>
-                                        <CustomRouterProvider relayEnv={environment} />
-                                    </AuthContextController>
-                                {/*</ModalContextProvider>*/}
-                            {/*</SnackBarProvider>*/}
-                        </React.Suspense>
-                    </ThemeProvider>
-                </LocalizationProvider>
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='en-gb'>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <React.Suspense fallback={<Loading />}>
+                        {/*<SnackBarProvider>*/}
+                        {/*    <ModalContextProvider>*/}
+                        <AuthContextController resetEnvironment={resetEnvironment}>
+                            <CustomRouterProvider relayEnv={environment} />
+                        </AuthContextController>
+                        {/*</ModalContextProvider>*/}
+                        {/*</SnackBarProvider>*/}
+                    </React.Suspense>
+                </ThemeProvider>
+            </LocalizationProvider>
         </RelayEnvironmentProvider>
     );
 }
--- src/Hooks/useRelayEnv.ts
+++ src/Hooks/useRelayEnv.ts
@@ -1,43 +1,60 @@
 import {
     Environment,
     Network,
-    RecordSource,
-    Store,
+    RecordSource, RequestParameters,
+    Store, Variables,
 } from 'relay-runtime';
+import React from "react";
 
-// Define a function that fetches the results of an operation (query/mutation/etc)
-// and returns its results as a Promise:
-function fetchQuery(
-    operation,
-    variables,
-    cacheConfig,
-    uploadables,
-) {
-    return fetch('https://localhost:5001/graphql', {
-        method: 'POST',
-        credentials: "include",
-        headers: {
-            // Add authentication and other headers here
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-            query: operation.text, // GraphQL text from input
-            variables,
-        }),
-    }).then(response => {
-        console.log(response);
-        return response.json();
+export function useRelayEnv() {
+    const [environment, setEnvironment] = React.useState(() =>
+        createEnvironment()
+    );
+    const [version, setVersion] = React.useState(0);
+
+    const resetEnvironment = React.useCallback(() => {
+        const newEnvironment = createEnvironment();
+        setEnvironment(newEnvironment);
+        setVersion((prevVersion) => prevVersion + 1);
+    }, [setEnvironment, setVersion]);
+
+    return {environment, resetEnvironment, version};
+}
+
+function createEnvironment() {
+    function fetchQuery(
+        operation: RequestParameters,
+        variables: Variables,
+    ) {
+        return fetch('https://localhost:5001/graphql', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                query: operation.text, // GraphQL query string
+                variables,
+            }),
+        }).then(response => {
+            console.log(response);
+            return response.json();
+        });
+    }
+
+    const network = Network.create(
+        fetchQuery
+    );
+
+    const store = new Store(new RecordSource());
+
+    return new Environment({
+        network,
+        store,
     });
 }
 
+// Define a function that fetches the results of an operation (query/mutation/etc)
+// and returns its results as a Promise:
+
 // Create a network layer from the fetch function
-const network = Network.create(fetchQuery);
-const store = new Store(new RecordSource())
-
-const environment = new Environment({
-    network,
-    store
-    // ... other options
-});
-
-export default environment;
