d3e4db07984ffe3b16cac4a6a3b95b16df4696c5
Wiaan Duvenhage <wiaan@stackworx.io>
2025-08-02 13:28:20 +0200

    chore: added dock for mobile

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,98 +1,97 @@
-import {Avatar, Button, Grid2, Link as RouterLink} from '@mui/material';
-import Logo from '../../assets/logo.svg';
-import {Link} from 'react-router-dom';
-import {useRouteMatch} from '../../Hooks/useRouteMatch.ts';
-import {useAuthContext} from '../../Context/AuthContext.tsx';
-import {deepOrange} from '@mui/material/colors';
-import {graphql, useMutation} from 'react-relay';
-import {HeaderMutation} from './__generated__/HeaderMutation.graphql.ts';
-import {useNavigate} from 'react-router';
+import { Avatar, Button, Grid2, Link as RouterLink } from "@mui/material";
+import Logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { useRouteMatch } from "../../Hooks/useRouteMatch.ts";
+import { useAuthContext } from "../../Context/AuthContext.tsx";
+import { deepOrange } from "@mui/material/colors";
+import { graphql, useMutation } from "react-relay";
+import { HeaderMutation } from "./__generated__/HeaderMutation.graphql.ts";
+import { useNavigate } from "react-router";
 
 export const Header = () => {
-  const routeMatches = useRouteMatch(['/register']);
-  const {resetEnvironment} = useAuthContext();
+  const routeMatches = useRouteMatch(["/register"]);
+  const { resetEnvironment } = useAuthContext();
   const navigate = useNavigate();
-  const {authenticated, me} = useAuthContext();
+  const { authenticated, me } = useAuthContext();
   const [mutate] = useMutation<HeaderMutation>(graphql`
-        mutation HeaderMutation($input: LogoutInput!) {
-            logout(input: $input) {
-                boolean
-            }
-        }
-    `);
+    mutation HeaderMutation($input: LogoutInput!) {
+      logout(input: $input) {
+        boolean
+      }
+    }
+  `);
 
   const handleLogout = () => {
     mutate({
-      variables: {input: {email: me?.email ?? '', password: ''}},
+      variables: { input: { email: me?.email ?? "", password: "" } },
       onCompleted: () => {
         resetEnvironment();
-        navigate('/');
+        navigate("/");
       },
     });
   };
-  return (
-    !routeMatches
-      ? (
-        <Grid2
-          size={12}
-          container
-          justifyContent={'space-between'}
-          alignItems={'center'}
-          position={'sticky'}
-          top={0}
-          zIndex={999}
-          p={3}
-          sx={{
-            background: 'rgba(255, 255, 255, 0.45)',
-            backdropFilter: 'blur(6.5px)',
-            WebkitBackdropFilter: 'blur(6.5px)',
-            border: '1px solid rgba(255, 255, 255, 0.18)',
-          }}
+  return !routeMatches ? (
+    <Grid2
+      size={12}
+      container
+      height={'10%'}
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      position={"sticky"}
+      top={0}
+      zIndex={999}
+      p={3}
+      sx={{
+        background: "rgba(255, 255, 255, 0.45)",
+        backdropFilter: "blur(6.5px)",
+        WebkitBackdropFilter: "blur(6.5px)",
+        border: "1px solid rgba(255, 255, 255, 0.18)",
+        display: {
+          xs: "none",
+          lg: "flex",
+        },
+      }}
+    >
+      <Grid2>
+        <img src={Logo} alt={"logo"} />
+      </Grid2>
+      <Grid2 container size={"auto"} columnGap={2} alignItems={"center"}>
+        <RouterLink
+          fontSize={"14px"}
+          fontWeight={"light"}
+          underline={"hover"}
+          component={Link}
+          to={"/"}
         >
-          <Grid2>
-            <img src={Logo} alt={'logo'} />
-          </Grid2>
-          <Grid2 container size={'auto'} columnGap={2} alignItems={'center'}>
-            <RouterLink
-              fontSize={'14px'}
-              fontWeight={'light'}
-              underline={'hover'}
-              component={Link}
-              to={'/'}
-            >
-              Home
+          Home
+        </RouterLink>
+        <RouterLink
+          fontSize={"14px"}
+          fontWeight={"light"}
+          underline={"hover"}
+          variant={"caption"}
+          component={Link}
+          to={"/listings"}
+        >
+          Listings
+        </RouterLink>
+        {authenticated ? (
+          <>
+            <RouterLink>
+              <Avatar sx={{ bgcolor: deepOrange[500] }}>
+                {me?.email?.charAt(0)}
+              </Avatar>
             </RouterLink>
-            <RouterLink
-              fontSize={'14px'}
-              fontWeight={'light'}
-              underline={'hover'}
-              variant={'caption'}
-              component={Link}
-              to={'/listings'}
+            <Button
+              title={"Log out"}
+              variant={"outlined"}
+              onClick={handleLogout}
             >
-              Listings
-            </RouterLink>
-            {authenticated
-              ? (
-                <>
-                  <RouterLink>
-                    <Avatar sx={{bgcolor: deepOrange[500]}}>
-                      {me?.email?.charAt(0)}
-                    </Avatar>
-                  </RouterLink>
-                  <Button
-                    title={'Log out'}
-                    variant={'outlined'}
-                    onClick={handleLogout}
-                  >
-                    Log out
-                  </Button>
-                </>
-              )
-              : null}
-          </Grid2>
-        </Grid2>
-      )
-      : null
-  );
+              Log out
+            </Button>
+          </>
+        ) : null}
+      </Grid2>
+    </Grid2>
+  ) : null;
 };
--- src/Layouts/RootLayout.tsx
+++ src/Layouts/RootLayout.tsx
@@ -1,15 +1,56 @@
-import React from 'react';
-import {Grid2} from '@mui/material';
-import {Outlet} from 'react-router-dom';
-import {Header} from '../Components/Header/Header.tsx';
+import React from "react";
+import { Grid2 } from "@mui/material";
+import { Outlet, useNavigate } from "react-router-dom";
+import { Header } from "../Components/Header/Header.tsx";
+import Dock from "../Components/Dock/Dock.tsx";
+import { Dashboard, House, Person } from "@mui/icons-material";
 
 export const RootLayout: React.FC = () => {
+  const navigate = useNavigate();
+  const items = [
+    { icon: <Dashboard />, label: "Home", onClick: () => navigate('/')},
+    {
+      icon: <House />,
+      label: "Listings",
+      onClick: () => navigate("listings"),
+    },
+    {
+      icon: <Person />,
+      label: "Profile",
+      onClick: () => navigate('profile'),
+    },
+  ];
+
   return (
-    <>
+    <Grid2 container size={12}>
       <Header />
-      <Grid2 p={5} size={12}>
+      <Grid2 p={5} size={12} overflow={"auto"}>
         <Outlet />
       </Grid2>
-    </>
+      <Grid2
+        size={{ xs: 12, lg: 0 }}
+        sx={{
+          display: {
+            xs: "block",
+            sm: "block",
+            md: "block",
+            lg: "none",
+            xl: "none",
+          },
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          zIndex: 1300,
+        }}
+      >
+        <Dock
+          items={items}
+          panelHeight={68}
+          baseItemSize={50}
+          magnification={50}
+        />
+      </Grid2>
+    </Grid2>
   );
 };
